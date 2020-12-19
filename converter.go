64507d5f20f34f26765abee5760ddf5d7c16f1d2
linwenlong <linwenlong@bytedance.com>
2020-12-19 12:27:16 +0800

    fix 5
    Change-Id: I13e1a54ceac9252f213996c6e9e4337b25289370

--- converter.go
+++ converter.go
@@ -25,6 +25,47 @@ type Converter struct {
 	open    bool
 }
 
+func Convert(input []byte, output []byte, fromEncoding string, toEncoding string) (bytesRead int, bytesWritten int, err error) {
+	// create a temporary converter
+	converter, err := NewConverter(fromEncoding, toEncoding)
+
+	if err == nil {
+		// call converter's Convert
+		bytesRead, bytesWritten, err = converter.Convert(input, output)
+
+		if err == nil {
+			var shiftBytesWritten int
+
+			// call Convert with a nil input to generate any end shift sequences
+			_, shiftBytesWritten, err = converter.Convert(nil, output[bytesWritten:])
+
+			// add shift bytes to total bytes
+			bytesWritten += shiftBytesWritten
+		}
+
+		// close the converter
+		converter.Close()
+	}
+
+	return
+}
+
+// All in one ConvertString method, rather than requiring the construction of an iconv.Converter
+func ConvertString(input string, fromEncoding string, toEncoding string) (output string, err error) {
+	// create a temporary converter
+	converter, err := NewConverter(fromEncoding, toEncoding)
+
+	if err == nil {
+		// convert the string
+		output, err = converter.ConvertString(input)
+
+		// close the converter
+		converter.Close()
+	}
+
+	return
+}
+
 // Initialize a new Converter. If fromEncoding or toEncoding are not supported by
 // iconv then an EINVAL error will be returned. An ENOMEM error maybe returned if
 // there is not enough memory to initialize an iconv descriptor
